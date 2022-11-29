8af08d69bebd3bc784f6632cedcfde6273f19bed
yojulab <otter.oh@gmail.com>
2022-11-29 09:12:57 +0900

    add event_propagations

---
+++ docs/browser_js/js/event_propagations.js
@@ -0,0 +1,36 @@
+// document.querySelector('#item1')
+// document.querySelector('#item2')
+
+// let queryInputAll = document.querySelectorAll("input");
+// for (let queryInput of queryInputAll) {
+//   queryInput.addEventListener("click", (event) => {
+//     alert(`clicked ${event.target.id} : ${event.target.value}`);
+//   });
+// }
+
+let newItem = `<li>
+                <div>
+                    <input type="checkbox" name="" id="item3">
+                    <label for="item3">item three</label>
+                </div>
+            </li>`;
+
+let queryItemList = document.querySelector(".item-list");
+queryItemList.addEventListener("click", (event) => {
+  if (event.target.id != "" && event.target.id != "undefined") {
+    alert(`clicked ${event.target.id} : ${event.target.value}`);
+  }
+  if (event.target.innerHTML == "delete") {
+    event.target.remove();
+  }
+}); //상위 element에 Event 설정
+// queryItemList.innerHTML = queryItemList.innerHTML + newItem;
+// queryItemList.insertAdjacentHTML("beforeend", newItem);
+// queryItemList.insertAdjacentHTML("beforebegin", newItem);
+
+// let queryInputs = document.querySelectorAll("input");
+// for (let queryInput of queryInputs) {
+//   queryInput.addEventListener("click", (event) => {
+//     alert(`clicked ${event.target.id} : ${event.target.value}`);
+//   });
+// }
---
+++ docs/browser_js/js/event_targets.js
@@ -0,0 +1,32 @@
+let querySingle = document.querySelector("#single");
+// querySingle.addEventListener("click", singleEvent);
+querySingle.addEventListener("click", (event) => {
+  singleEvent(event);
+});
+// querySingle.addEventListener("double-click", singleEvent);
+
+function singleEvent(event) {
+  console.log(event);
+  querySingle.innerHTML = "Take One Event!";
+}
+
+let queryKeydown = document.querySelector("#keydown");
+queryKeydown.addEventListener("keydown", (event) => {
+  console.log(event);
+  if (event.code == "Enter") {
+    keydownDesc(queryKeydown.value);
+  }
+  console.log(event.target);
+});
+
+function keydownDesc(desc) {
+  let queryKeydownDesc = document.querySelector("#keydownDesc");
+  queryKeydownDesc.innerHTML = desc;
+}
+
+let queryload = document.querySelector("#keydownDesc");
+queryload.addEventListener("load", queryLoad());
+
+function queryLoad() {
+  queryload.innerHTML = "Done On Load!";
+}
